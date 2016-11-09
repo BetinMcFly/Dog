3c27b78bb292997e675341c8aff803dccf044687
leno01-PC\asolano <carlos.solano@tecnoaplicada.com>
2016-11-08 21:44:10 -0600

    Primer commit

---
+++ src/com/carlosalberto/dog/Dog.java
@@ -0,0 +1,22 @@
+package com.carlosalberto.dog;
+
+class Dog{
+	  //Variables de instacia
+	  int age;
+	  
+	  //Constructor
+	  public Dog(int dogsAge) {
+	    age = dogsAge;
+	  }
+
+
+
+		//Metodo principal
+		public static void main(String[] args) {
+	    
+	    Dog spike = new Dog(6);
+	    
+
+		}
+
+	}
